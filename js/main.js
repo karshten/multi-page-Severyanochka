14631f378905cadcf9c1072ebb0fd6ca30a893dd
oskar shtefan <oskar.shtefan@gmail.com>
2022-03-25 17:03:52 +0600

    added changes in stocks section

--- js/main.js
+++ js/main.js
@@ -1,29 +1,31 @@
 const signInBtn = document.querySelector('#sign-in-btn')
 const signInModalForm = document.querySelector('.sign-in-modalJS')
-const signInDialog = document.querySelector('.sign-in-modal__dialog')
 
-const showModal = () => {
-    signInModalForm.classList.replace('hide', 'sign-in-modal')
+const showModal = (modal) => {
+    modal.classList.replace('hide', 'sign-in-modal')
     document.body.style.overflow = 'hidden'
 }
 
-const hideModal = () => {
-    signInModalForm.classList.replace('sign-in-modal', 'hide')
+const hideModal = (modal) => {
+    modal.classList.replace('sign-in-modal', 'hide')
     document.body.style.overflow = 'scroll'
 }
 
 signInBtn.onclick = () => {
-    showModal()
+    showModal(signInModalForm)
 }
 
-signInModalForm.addEventListener('click', (e)=>{
-    if (e.target.classList.contains('sign-in-modalJS')){
-        hideModal()
-    }
-})
+const hideModalOnEvent = (modal, modalClass) => {
+    modal.addEventListener('click', (e) => {
+        if (e.target.classList.contains(modalClass)) {
+            hideModal(modal)
+        }
+    })
+}
+hideModalOnEvent(signInModalForm, 'sign-in-modalJS')
 
 document.body.addEventListener('keydown', (e) => {
     if (e.code === "Escape") {
-        hideModal()
+        hideModal(signInModalForm)
     }
 })
